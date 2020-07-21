93a9d3396d33c122e1f2e6cdc13d51f8c1848251
zlatantucakovic <zlatan.tucakovic@gmail.com>
2020-07-21 15:21:46 +0200

    Changes

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,4 +24,3 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-sudo docker build -t jovan:1.0 .
